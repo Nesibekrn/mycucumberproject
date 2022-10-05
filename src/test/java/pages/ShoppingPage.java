fc6bbfc323a1de4bc7a5639e4727ce78166e1642
Nesibe <nesibekrn@gmail.com>
2022-10-05 14:10:06 +0300

    first commit

--- src/test/java/pages/ShoppingPage.java
+++ src/test/java/pages/ShoppingPage.java
@@ -3,11 +3,11 @@ package pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import utilities.Driver;
+//import utilities.Driver;
 
 public class ShoppingPage {
     public ShoppingPage(){
-        PageFactory.initElements(Driver.getDriver(),this);
+        //PageFactory.initElements(Driver.getDriver(),this);
     }
     @FindBy(xpath = "//a[@href='/login']")
     public WebElement signupLoginButton;
